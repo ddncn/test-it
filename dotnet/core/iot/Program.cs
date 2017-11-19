86ae93f8a7938ab519f2dacaea6302255d4220cf
dduncan <>
2017-11-18 23:07:45 -0600

    Adding iot example
     Adding files

---
+++ dotnet/core/iot/Program.cs
@@ -0,0 +1,16 @@
+﻿// [How to create, deploy and run a .NET Core 2.0 app on a Raspberry Pi 3 ARM processor](https://jeremylindsayni.wordpress.com/2017/03/20/how-to-create-deploy-and-run-a-net-core-2-0-app-on-a-raspberry-pi-3-arm-processor/)
+// [Is there any documentations to install .Net Core on Windows IoT Core?](https://github.com/dotnet/core/issues/232)
+// [Create a Web API with ASP.NET Core MVC and Visual Studio Code on Linux, macOS, and Windows](https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc)
+
+using System;
+
+namespace iot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
